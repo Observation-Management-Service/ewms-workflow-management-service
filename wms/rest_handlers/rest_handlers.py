f28ae1c338970235ae9bcc9a326b45eea00e3dbf
Ric Evans <ric@evans-jacquez.com>
2024-01-29 16:46:18 -0600

    mv rest_handlers.py

--- wms/rest_handlers/rest_handlers.py
+++ wms/rest_handlers/rest_handlers.py
@@ -99,3 +99,18 @@ class MainHandler(BaseWMSHandler):  # pylint: disable=W0223
 
 
 # -----------------------------------------------------------------------------
+
+
+f"/tms/job-event-log/{urllib.parse.quote(str(jel_fpath),safe='')}",
+
+
+f"/tms/taskforce/running/{args['taskforce_uuid']}",
+
+f"/tms/taskforce/stop/{args['taskforce_uuid']}",
+
+f"/tms/taskforce/{taskforce_uuid}",
+
+"/tms/taskforces/find"
+"/tms/taskforce/pending"
+"/tms/taskforces/report"
+"/tms/taskforce/stop"
