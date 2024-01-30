52ff97300895686904916dfaa063220b4cb2ef3d
Ric Evans <ric@evans-jacquez.com>
2024-01-30 17:47:10 -0600

    convert schema handler

--- wms/rest_handlers/schema_handlers.py
+++ wms/rest_handlers/schema_handlers.py
@@ -2,34 +2,19 @@
 
 
 import logging
-from typing import Any
 
-from ..schema import rest_schema
 from . import auth
 from .base_handlers import BaseWMSHandler
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_json_schema_handler(objective_route: str) -> type[BaseWMSHandler]:
-    """Create a route handler for getting the JSON-schema for route."""
-    if not objective_route.startswith("/"):
-        raise ValueError(f"objective_route must start with '/': {objective_route}")
+class SchemaHandler(BaseWMSHandler):  # pylint: disable=W0223
+    """The sole handler for retrieving the OpenAPI schema."""
 
-    try:
-        this_schema = rest_schema.RestSchema[objective_route]
-    except KeyError:
-        LOGGER.warning(f"this route does not have a JSON-schema: {objective_route}")
-        raise
+    ROUTE = r"/schema/openapi$"
 
-    class JSONSchemaHandler(BaseWMSHandler):  # pylint: disable=W0223
-        ROUTE = f"/schema{objective_route}"
-
-        @auth.service_account_auth(roles=auth.ALL_AUTH_ACCOUNTS)  # type: ignore
-        async def get(self, *args: Any, **kwargs: Any) -> None:
-            self.write(this_schema)
-
-    return JSONSchemaHandler
-
-
-# ----------------------------------------------------------------------------
+    @auth.service_account_auth(roles=auth.ALL_AUTH_ACCOUNTS)  # type: ignore
+    async def get(self) -> None:
+        """Handle GET."""
+        self.write({})
