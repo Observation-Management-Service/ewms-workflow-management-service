455cacec6345ac90e9fc4b546568569da4f131c2
Ric Evans <ric@evans-jacquez.com>
2024-01-30 14:51:49 -0600

    stub out `BacklogMongoClient`

--- wms/database/backlog.py
+++ wms/database/backlog.py
@@ -1,4 +1,4 @@
-"""Interface to task directive collection in the mongo database."""
+"""Interface to backlog collection in the mongo database."""
 
 
 import logging
@@ -8,8 +8,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 LOGGER = logging.getLogger(__name__)
 
 
-class TaskDirectiveClient:
-    """A client for interacting with task directives."""
+class BacklogMongoClient:
+    """A client for interacting with backlog entries."""
 
     def __init__(self, mongo_client: AsyncIOMotorClient) -> None:  # type: ignore[valid-type]
         self.mongo_client = mongo_client
