265f10b0972b3be0902459eab6d751390d21f018
Ric Evans <ric@evans-jacquez.com>
2024-01-30 17:40:42 -0600

    use `.json` files for schemas, and load in `config.py`

--- wms/config.py
+++ wms/config.py
@@ -2,13 +2,55 @@
 
 
 import dataclasses as dc
+import json
 import logging
+from pathlib import Path
 from typing import Any
 
+import jsonschema
+import referencing
+from openapi_core import OpenAPI  # type: ignore[attr-defined]
+from openapi_spec_validator import validate
+from openapi_spec_validator.readers import read_from_filename
 from wipac_dev_tools import from_environment_as_dataclass, logging_tools
 
+LOGGER = logging.getLogger(__name__)
+
+
 # --------------------------------------------------------------------------------------
-# Constants
+
+
+def _get_json_schema_specs(fpath: Path) -> dict[str, jsonschema.protocols.Validator]:
+    with open(fpath) as f:
+        dicto = json.load(f)
+    for key, entry in dicto.items():
+        LOGGER.info(f"validating JSON-schema spec for {key} ({fpath})")
+        jsonschema.protocols.Validator.check_schema(entry)
+    registry = referencing.jsonschema.SchemaRegistry()  # possible future dev
+    return {k: jsonschema.protocols.Validator(v, registry) for k, v in dicto.items()}
+
+
+DATABASE_JSON_SCHEMA_LOOKUP = _get_json_schema_specs(
+    Path(__file__).parent / "schema/database_schema.json"
+)
+
+
+# --------------------------------------------------------------------------------------
+
+
+def _get_openapi_spec(fpath: Path) -> OpenAPI:
+    spec_dict, base_uri = read_from_filename(str(fpath))
+    LOGGER.info(f"validating OpenAPI spec for {base_uri} ({fpath})")
+    validate(spec_dict)  # no exception -> spec is valid
+    return OpenAPI.from_file_path(fpath)
+
+
+REST_OPENAPI_SPEC = _get_openapi_spec(
+    Path(__file__).parent / "schema/rest_openapi.json"
+)
+
+
+# --------------------------------------------------------------------------------------
 
 
 @dc.dataclass(frozen=True)
@@ -38,6 +80,9 @@ class EnvConfig:
 ENV = from_environment_as_dataclass(EnvConfig)
 
 
+# --------------------------------------------------------------------------------------
+
+
 # known cluster locations
 KNOWN_CLUSTERS: dict[str, dict[str, Any]] = {
     "sub-2": {
@@ -47,6 +92,9 @@ KNOWN_CLUSTERS: dict[str, dict[str, Any]] = {
         },
     },
 }
+
+
+# --------------------------------------------------------------------------------------
 
 
 def is_testing() -> bool:
