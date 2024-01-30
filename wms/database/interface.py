ba070c0cbd197b47562539c25423aa17ef346705
Ric Evans <ric@evans-jacquez.com>
2024-01-30 14:47:31 -0600

    stub out `TaskDirectivesClient`

--- wms/database/interface.py
+++ wms/database/interface.py
@@ -3,4 +3,13 @@
 
 import logging
 
+from motor.motor_asyncio import AsyncIOMotorClient
+
 LOGGER = logging.getLogger(__name__)
+
+
+class TaskDirectivesClient:
+    """A client for interacting with task directives."""
+
+    def __init__(self, mongo_client: AsyncIOMotorClient) -> None:  # type: ignore[valid-type]
+        self.mongo_client = mongo_client
