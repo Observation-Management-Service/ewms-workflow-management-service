3ebef89684dcd942e7502d4714558963efb1047e
Ric Evans <ric@evans-jacquez.com>
2024-02-05 14:27:20 -0600

    move mongo to gha `services`

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,7 +3,6 @@
 
 import asyncio
 import dataclasses
-import itertools
 import logging
 import os
 from typing import AsyncIterator
@@ -26,24 +25,6 @@ async def rc() -> AsyncIterator[RestClient]:
 
     Cleanup when iterator resumes.
     """
-
-    with open(os.environ["CI_MONGO_STDOUT"], "wb") as stdoutf, open(
-        os.environ["CI_MONGO_STDERR"], "wb"
-    ) as stderrf:
-        cmd = (
-            f"docker run --network='host' --rm {os.environ['CI_MONGO_IMAGE_W_TAG']}"
-        )
-        LOGGER.info(f"running: {cmd}")
-        mongo_task = asyncio.create_task(
-            (
-                await asyncio.create_subprocess_shell(
-                    cmd,
-                    stdout=stdoutf,
-                    stderr=stderrf,
-                )
-            ).wait()
-        )
-
     with open(os.environ["CI_REST_STDOUT"], "wb") as stdoutf, open(
         os.environ["CI_REST_STDERR"], "wb"
     ) as stderrf:
@@ -66,27 +47,10 @@ async def rc() -> AsyncIterator[RestClient]:
 
     await asyncio.sleep(0)  # start up tasks
 
-    # ping until all live
-    for hostname in [
-        f'{os.environ["MONGODB_HOST"]}:{os.environ["MONGODB_PORT"]}',
-        f'{os.environ["REST_HOST"]}:{os.environ["REST_PORT"]}',
-    ]:
-        LOGGER.info(f"waiting for host: {hostname}")
-        for i in itertools.count():
-            if i > 10:
-                LOGGER.critical(f"could not connect to {hostname}")
-                break
-            if os.system("ping -c 1 " + hostname) == 0:
-                LOGGER.info(f"reached host: {hostname}")
-                break
-            LOGGER.info(f"{hostname}...")
-            await asyncio.sleep(1)
-
     yield RestClient(
         f'{os.environ["REST_HOST"]}:{os.environ["REST_PORT"]}',
         timeout=3,
         retries=2,
     )
 
-    mongo_task.cancel()
     rest_task.cancel()
