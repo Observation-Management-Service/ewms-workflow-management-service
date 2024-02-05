da20e3d155e7743aa715054664cf8bf493df5766
Ric Evans <ric@evans-jacquez.com>
2024-02-05 15:21:01 -0600

    add sleep after rest server startup

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,21 +1,14 @@
 """conftest.py."""
 
 
-import dataclasses
 import logging
 import os
 from typing import AsyncIterator
 
 import pytest_asyncio
 from rest_tools.client import RestClient
-from wms import config  # only use to access constants
 
 LOGGER = logging.getLogger(__name__)
-
-
-FORWARD_ENVVARS = [
-    f.name for f in dataclasses.fields(config.EnvConfig) if f.name in os.environ
-]
 
 
 @pytest_asyncio.fixture
