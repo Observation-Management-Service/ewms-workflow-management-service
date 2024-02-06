9c70e1f809b8ebec3eb36ec41b712faaccf0be87
Ric Evans <ric@evans-jacquez.com>
2024-02-06 14:20:54 -0600

    test `/schema/openapi` - assert return

--- tests/integration/test_tms_workflow.py
+++ tests/integration/test_tms_workflow.py
@@ -1,6 +1,9 @@
 """Mimic a TMS workflow, hitting the expected REST endpoints."""
 
 
+import json
+from pathlib import Path
+
 from rest_tools.client import RestClient
 
 
@@ -9,5 +12,7 @@ async def test_000(rc: RestClient) -> None:
     resp = await rc.request("GET", "/schema/openapi")
     # TODO - use openapi to validate response client-side (not done server side)
     print(resp)
+    with open(Path(__file__).parent / "../../wms/schema/rest_openapi.json", "rb") as f:
+        assert json.load(f) == resp
 
     assert 0
