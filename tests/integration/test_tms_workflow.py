76578b2d3edc67afdce87169535aea0c7843cf03
Ric Evans <ric@evans-jacquez.com>
2024-02-06 16:36:33 -0600

    pt 3 (wip)

--- tests/integration/test_tms_workflow.py
+++ tests/integration/test_tms_workflow.py
@@ -13,6 +13,20 @@ _OPENAPI_JSON = Path(__file__).parent / "../../wms/schema/rest_openapi.json"
 _OPENAPI_SPEC = openapi_core.Spec.from_file_path(str(_OPENAPI_JSON))
 
 
+def validate_response(response: requests.Response) -> None:
+    """Validate using 'requests' types."""
+
+    # duck typing magic
+    openapi_resp = openapi_core_requests.RequestsOpenAPIResponse(response)
+    openapi_resp.headers = dict(openapi_resp.headers)
+
+    openapi_core.validate_response(
+        openapi_core_requests.RequestsOpenAPIRequest(response.request),
+        openapi_resp,  # type: ignore[arg-type]
+        _OPENAPI_SPEC,
+    )
+
+
 async def test_000(rc: RestClient) -> None:
     """Regular workflow."""
     resp: requests.Response = requests.get(rc.address + "/schema/openapi")
@@ -20,8 +34,4 @@ async def test_000(rc: RestClient) -> None:
     print(resp)
     with open(_OPENAPI_JSON, "rb") as f:
         assert json.load(f) == rc._decode(resp.content)
-    openapi_core.validate_response(
-        openapi_core_requests.RequestsOpenAPIRequest(resp.request),
-        openapi_core_requests.RequestsOpenAPIResponse(resp),  # type: ignore[arg-type]
-        _OPENAPI_SPEC,
-    )
+    validate_response(resp)
