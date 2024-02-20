a45d9845b79f603646da16f1a07f1272145cbfbb
Ric Evans <ric@evans-jacquez.com>
2024-02-20 16:48:24 -0600

    ci: move common utility to own file

--- resources/modify_rest_path_schemas.py
+++ resources/modify_rest_path_schemas.py
@@ -4,7 +4,8 @@
 import json
 import logging
 import pathlib
-from typing import Callable
+
+from set_all_nested import set_all_nested
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,35 +24,6 @@ NEW_400 = {
         }
     },
 }
-
-
-def set_all_nested(
-    spec: dict,
-    setter: Callable[[dict, str], None],
-    if_this: Callable[[dict, str], bool],
-) -> dict:
-    """Call 'setter' for every key where 'if_this' is True."""
-
-    def settle_nested_subdict_one_at_a_time(subdict: dict):
-        """Calling repeatedly will complete the settling.
-
-        Will return without error if nothing was changed.
-        """
-        for key, val in subdict.items():
-            if if_this(subdict, key):
-                setter(subdict, key)
-                raise Exception("dict may have changed (did not check prev value)")
-            elif isinstance(val, dict):
-                settle_nested_subdict_one_at_a_time(val)
-
-    while True:
-        try:
-            settle_nested_subdict_one_at_a_time(spec)
-        except:  # noqa: E722
-            continue
-        break
-
-    return spec
 
 
 def main() -> None:
