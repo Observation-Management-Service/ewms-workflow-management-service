2b7e3a27192ba5b4f583d3cae991458bd35115a6
Ric Evans <ric@evans-jacquez.com>
2024-02-21 15:12:26 -0600

    pt 2

--- wms/server.py
+++ wms/server.py
@@ -21,8 +21,6 @@ HANDLERS = [
     rest_handlers.task_handlers.TaskDirectiveIDHandler,
     rest_handlers.task_handlers.TaskDirectivesFindHandler,
     #
-    rest_handlers.job_event_log_handlers.JobEventLogHandler,
-    #
     rest_handlers.taskforce_handlers.TaskforcesReportHandler,
     rest_handlers.taskforce_handlers.TaskforcesFindHandler,
     #
