7737f8e2ac67ea50ffb67c460c4c0ac46e1ea770
ric-evans <emejqz@gmail.com>
2024-05-28 14:02:08 -0500

    prefix `/v0/` - 3

--- wms/rest_handlers/schema_handlers.py
+++ wms/rest_handlers/schema_handlers.py
@@ -14,7 +14,7 @@ LOGGER = logging.getLogger(__name__)
 class SchemaHandler(BaseWMSHandler):  # pylint: disable=W0223
     """The sole handler for retrieving the OpenAPI schema."""
 
-    ROUTE = rf"/{ROUTE_VERSION_PREFIX}/schema/openapi$"
+    ROUTE = rf"/{config.ROUTE_VERSION_PREFIX}/schema/openapi$"
 
     @auth.service_account_auth(roles=auth.ALL_AUTH_ACCOUNTS)  # type: ignore
     @validate_request(config.REST_OPENAPI_SPEC)  # type: ignore[misc]
