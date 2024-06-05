a01c874a833cab2c0a43848dd9faef6ce8cb01e6
ric-evans <emejqz@gmail.com>
2024-06-05 15:59:15 -0400

    use README instead

--- examples/oneliner_request_workflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-# meant to be copy-pasted into terminal as one-liner, with tokens replaced
-
-export EWMS_PILOT_BROKER_ADDRESS="XXX"  &&  export KEYCLOAK_CLIENT_ID_BROKER="ewms-rabbitmq" &&  export KEYCLOAK_CLIENT_SECRET_BROKER=$(echo YYY | base64 --decode)  &&  python3 request_workflow.py --pilot-cvmfs-image-tag A.B.C
