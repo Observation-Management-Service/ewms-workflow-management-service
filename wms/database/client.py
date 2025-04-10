5704192c61b53a8a2c3231c0e8f7a0902e1380b7
ric-evans <emejqz@gmail.com>
2025-04-10 15:19:57 -0500

    use `validation_exception_callback`

--- wms/database/client.py
+++ wms/database/client.py
@@ -2,11 +2,13 @@
 
 import logging
 
+import jsonschema
 from motor.motor_asyncio import AsyncIOMotorClient
+from tornado import web
 from wipac_dev_tools.mongo_jsonschema_tools import (
     DocumentNotFoundException,
+    IllegalDotsNotationActionException,
     MongoJSONSchemaValidatedCollection,
-    MongoJSONSchemaValidationError,
 )
 
 from .utils import (
@@ -18,10 +20,28 @@ from .utils import (
 )
 from ..config import MONGO_COLLECTION_JSONSCHEMA_SPECS
 
-__all__ = [  # export this one
+__all__ = [  # export
     "DocumentNotFoundException",
-    "MongoJSONSchemaValidationError",
 ]
+
+
+def _validation_exception_callback(exc: Exception) -> Exception:
+    """Translate exceptions for web use."""
+    match exc:
+        case jsonschema.exceptions.ValidationError():
+            return web.HTTPError(
+                status_code=500,
+                log_message=f"{exc.__class__.__name__}: {exc}",  # to stderr
+                reason="Attempted to insert invalid data into database",  # to client
+            )
+        case IllegalDotsNotationActionException():
+            return web.HTTPError(
+                status_code=500,
+                log_message=f"{exc.__class__.__name__}: {exc}",  # to stderr
+                reason="Could not perform action on database with provided syntax",  # to client
+            )
+        case _:
+            return exc
 
 
 class WMSMongoValidatedDatabase:
@@ -39,6 +59,7 @@ class WMSMongoValidatedDatabase:
                 get_jsonschema_spec_name(WORKFLOWS_COLL_NAME)
             ],
             parent_logger,
+            validation_exception_callback=_validation_exception_callback,
         )
         self.task_directives_collection = MongoJSONSchemaValidatedCollection(
             mongo_client[_DB_NAME][TASK_DIRECTIVES_COLL_NAME],
@@ -46,6 +67,7 @@ class WMSMongoValidatedDatabase:
                 get_jsonschema_spec_name(TASK_DIRECTIVES_COLL_NAME)
             ],
             parent_logger,
+            validation_exception_callback=_validation_exception_callback,
         )
         self.taskforces_collection = MongoJSONSchemaValidatedCollection(
             mongo_client[_DB_NAME][TASKFORCES_COLL_NAME],
@@ -53,4 +75,5 @@ class WMSMongoValidatedDatabase:
                 get_jsonschema_spec_name(TASKFORCES_COLL_NAME)
             ],
             parent_logger,
+            validation_exception_callback=_validation_exception_callback,
         )
