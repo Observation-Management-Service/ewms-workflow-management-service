cedc5d54e6079defd5232410d8729a5f9d48795d
ric-evans <emejqz@gmail.com>
2026-03-12 11:30:45 -0500

    mypy

--- wms/config.py
+++ wms/config.py
@@ -102,9 +102,7 @@ def _get_openapi_spec(fpath: Path) -> tuple[openapi_core.OpenAPI, dict[str, Any]
 
 
 OPENAPI_SPEC, OPENAPI_DICT = _get_openapi_spec(OPENAPI_PATH)
-URL_V_PREFIX = (  # ex: v0
-    "v" + OPENAPI_SPEC.spec.contents()["info"]["version"].split(".", maxsplit=1)[0]
-)
+URL_V_PREFIX = "v" + OPENAPI_DICT["info"]["version"].split(".", maxsplit=1)[0]  # ex: v0
 
 
 # --------------------------------------------------------------------------------------
