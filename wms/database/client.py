b8a13cb303b1863a45c5660eaa061edce790f052
ric-evans <emejqz@gmail.com>
2026-03-12 15:17:34 -0500

    no more motor - 2

--- wms/database/client.py
+++ wms/database/client.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 import jsonschema
-from pymongo.asynchronous.collection import AsyncCollection
+from pymongo import AsyncMongoClient
 from tornado import web
 from wipac_dev_tools.mongo_jsonschema_tools import (
     DocumentNotFoundException,
@@ -64,7 +64,7 @@ class WMSMongoValidatedDatabase:
 
     def __init__(
         self,
-        mongo_client: AsyncCollection,
+        mongo_client: AsyncMongoClient,
         parent_logger: logging.Logger | None = None,
     ):
         self.mongo_client = mongo_client
